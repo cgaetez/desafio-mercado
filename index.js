590e8a7e36728f89db3fb6f4af9f1cf33b2a2251
cgaetez <claudiagaetez@gmail.com>
2024-05-28 10:43:03 +0200

    desafio

--- index.js
+++ index.js
@@ -13,16 +13,20 @@ app.set('views', path.join(__dirname + '/views'))
 app.engine('handlebars', 
 exphbs.engine({
     defaultLayout: "main",
-    //layoutsDir: __dirname + "/view/layouts",
+    layoutsDir: __dirname + "/views/layouts",
+    partialsDir: __dirname + "/views/partials"
+     
 }));
 
+//static
+app.use(express.static('assets'))
 
 app.get('/', (req,res) => {
     res.render('home',{
     title: '::: Home :::'
     })
 })
-
+/*
 app.get('/about', (req,res) => {
     res.render('about',{
         title: '::: Acerca de nosotros:::'
@@ -35,11 +39,64 @@ app.get('/contact', (req,res) => {
         })
     })
 
-app.get('/products', (req,res) => {
+*/
+//npm install jsonfile
+const jsonfile = require('jsonfile');
+const pathjson = './assets/productos.json';
+
+jsonfile.readFile(pathjson, (err, data) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log(data);
+  //console.log('test');    
+    app.get('/products', (req,res) => {
         res.render('products',{
-            title: '::: Productos :::'
+            title: '::: Productos :::',
+            productos: data.productos
             })
-})
+    })
+
+    app.use('/product/:producto', function (req, res, next) { 
+       // JSON.parse(data.productos).includes(req.params.producto)
+       let nombre = req.params.producto.toString();
+       //console.log('entrada');
+       let resp=0;
+        Object.entries(data.productos).forEach(([key, value]) => {
+            console.log('resp: ' + resp);
+            if(value.nombre == req.params.producto){
+                //console.log(value.nombre);
+                resp=1;
+                return false;
+            }
+
+      });
+        if(resp===1){
+            //console.log(value.nombre);
+            next();
+        }else{
+            console.log('else');
+            res.sendFile(__dirname + '/assets/img/noproduct.jpeg');
+        }
+
+    })
+
+    app.get("/product/:producto", (req, res) => {
+        console.log(req.params.producto);
+        res.render('product',{
+            title: '::: Producto :::',
+            producto: req.params.producto
+        })
+    });
+
+ });
+
+
+
+ 
+
+
 
 /*
 app.get('/home', (req,res) =>{
